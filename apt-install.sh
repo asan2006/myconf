c2a2140468dc764237e31f5eebc3a6e0517e6bc4
Robin Yu <bin___03@163.com>
2022-06-01 02:25:10 +0800

    fixed

--- apt-install.sh
+++ apt-install.sh
@@ -8,7 +8,7 @@ cd ${SCRIPT_PATH}
 
 sudo apt update
 sudo apt upgrade
-sudo apt install ssh vim git ranger
+sudo apt install ssh vim git ranger feh
 sudo apt install wget curl gnupg lsb-release net-tools
 sudo apt install neofetch terminator
 
@@ -18,3 +18,5 @@ sudo apt install compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus 
 #git config --global user.name "Robin Yu"
 #git config --global user.email "bin___03@163.com"
 #git config credential.helper store
+
+# feh --bg-fill ~/Pictures/warty-final-ubuntu.png
